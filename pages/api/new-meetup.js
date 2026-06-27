d5bed6964f3b605fbcc21ace21ae47c14cfabf8f
Mahesh Bhosale <maheshbhosale0507@gmail.com>
2026-06-27 21:27:14 +0200

    Adding mango db cluster setup with adding new meetup event in db

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.MONGODB_DBNAME}:${process.env.MONGODB_PASSWORD}@cluster0.pjmngk9.mongodb.net/?appName=Cluster0`,
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const AddNewMeetup = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
   };
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
